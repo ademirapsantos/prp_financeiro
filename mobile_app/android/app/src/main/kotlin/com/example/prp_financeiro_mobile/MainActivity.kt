cecf5fad2b1f38abc37e3e244886d5e2caf595d1
Ademir Santos <ademir574@gmail.com>
2026-02-27 14:33:42 -0300

    feat: Add biometric permissions and update app functionality; enhance logout feature and settings

--- mobile_app/android/app/src/main/kotlin/com/example/prp_financeiro_mobile/MainActivity.kt
+++ mobile_app/android/app/src/main/kotlin/com/example/prp_financeiro_mobile/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.prp_financeiro_mobile
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
